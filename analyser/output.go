2134cb682249e94dac5a04fa150d8af263dab103
luckelucky <officialluckelucky@gmail.com>
2022-09-08 19:49:48 +0100

    liquipedia format

--- analyser/output.go
+++ analyser/output.go
@@ -8,19 +8,63 @@ import (
 	"github.com/fatih/color"
 )
 
+func getSide(isCt bool) string {
+	if isCt {
+		return "ct"
+	}
+	return "t"
+}
+
+func printWithFormat(teamName string, firstHalf *Half, secondHalf *Half, index int) {
+	t1side := getSide(teamName == firstHalf.ctName)
+	//|t1firstside=|t1t=|t1ct=|t2t=|t2ct=
+	var format []string
+	if t1side == "ct" {
+		format = append(format,
+			color.BlueString(t1side),
+			color.RedString(strconv.Itoa(secondHalf.halfTScore)),
+			color.BlueString(strconv.Itoa(firstHalf.halfCtScore)),
+			color.RedString(strconv.Itoa(firstHalf.halfTScore)),
+			color.BlueString(strconv.Itoa(secondHalf.halfCtScore)),
+		)
+	} else if t1side == "t" {
+		format = append(format,
+			color.RedString(t1side),
+			color.RedString(strconv.Itoa(firstHalf.halfTScore)),
+			color.BlueString(strconv.Itoa(secondHalf.halfCtScore)),
+			color.RedString(strconv.Itoa(secondHalf.halfTScore)),
+			color.BlueString(strconv.Itoa(firstHalf.halfCtScore)),
+		)
+	}
+	key := ""
+	if index >= 2 {
+		key = strconv.Itoa(index / 2)
+		key = "o" + key
+	}
+	fmt.Fprintf(color.Output, "\t|"+key+"t1firstside=%s|"+
+		key+"t1t=%s|"+
+		key+"t1ct=%s|"+
+		key+"t2t=%s|"+
+		key+"t2ct=%s\n",
+		format[0], format[1], format[2], format[3], format[4])
+}
+
 func (analyser *Analyser) printHalfs() {
 	if len(analyser.halfs) == 0 {
 		return
 	}
 
-	var firstCtNme string
+	ctName := analyser.halfs[0].ctName
+	tName := analyser.halfs[0].tName
 
-	for i, half := range analyser.halfs {
-		if i == 0 {
-			firstCtNme = half.ctName
-			fmt.Printf("Half  |%.6s|%.6s\n", utils.PadSpaceEnd(half.ctName, 6), utils.PadSpaceEnd(half.tName, 6))
-		}
-		printScoresHalf(half, i+1, half.ctName == firstCtNme)
+	fmt.Printf("Team1:%s\n", ctName)
+	for i := 0; i < len(analyser.halfs); i = i + 2 {
+		printWithFormat(ctName, analyser.halfs[i], analyser.halfs[i+1], i)
+	}
+
+	fmt.Printf("Team1:%s\n", tName)
+	for i := 0; i < len(analyser.halfs); i = i + 2 {
+		printWithFormat(tName, analyser.halfs[i], analyser.halfs[i+1], i)
 	}
 }
 
@@ -44,22 +88,6 @@ func (analyser *Analyser) printRoundsPlayed() {
 	fmt.Printf("Rounds played:%d\n", analyser.roundsPlayed)
 }
 
-func printScoresHalf(half *Half, nHalf int, isCTOnLeft bool) {
-	ctScore := utils.PadSpaceEnd(strconv.Itoa(half.halfCtScore), 6)
-	tScore := utils.PadSpaceEnd(strconv.Itoa(half.halfTScore), 6)
-
-	halfPrint := utils.PadSpaceEnd(strconv.Itoa(nHalf), 6)
-	format := []string{color.RedString(tScore), color.BlueString(ctScore)}
-	if isCTOnLeft {
-		format[0], format[1] = format[1], format[0]
-	}
-	fmt.Printf("%s|", halfPrint)
-	if utils.IsWindows() {
-		fmt.Fprintf(color.Output, "%s|%s\n", format[0], format[1])
-	} else {
-		fmt.Printf("%s|%s\n", format[0], format[1])
-	}
-}
 func (analyser *Analyser) printScore() {
 	ctName := analyser.parser.GameState().TeamCounterTerrorists().ClanName()
 	tName := analyser.parser.GameState().TeamTerrorists().ClanName()
